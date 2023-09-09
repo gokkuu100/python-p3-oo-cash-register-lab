0c8ab8dbd197b7a26e2179e5b0c6866fcae9f805
goku <wkurts247@gmail.com>
2023-09-09 09:30:13 +0300

    Completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self):
+        self.total = 0 
+        self.discount = 0  
+        self.last_transaction = 0  
+
+    def add_item(self, item_price):
+        self.total += item_price
+        self.last_transaction = item_price
+
+    def apply_discount(self, discount_percentage):
+        discount_factor = float(discount_percentage) / 100
+
+        self.total -= self.total * discount_factor
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction
+
+
+register = CashRegister()
+
+register.add_item(10)
+register.add_item(15)
+
+print("total", register.total)
